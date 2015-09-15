e7677e4423a6bb25a8ec7d77526496550f490c1b
Chris LaRose <chris.larose@mycase.com>
2015-09-14 23:01:25 -0700

    cjl - Add basic belt element type

--- src/Main.purs
+++ src/Main.purs
@@ -1,6 +1,16 @@
 module Main where
 
 import Control.Monad.Eff.Console
+import Prelude
+
+data BeltElement = NaR Int | Result Int
+
+instance showBeltElement :: Show BeltElement where
+  show (NaR x) = "NaR " ++ show x
+  show (Result x) = "Result " ++ show x
+
+addu :: Int -> Int -> BeltElement
+addu x y = Result (x + y)
 
 main = do
-  log "Hello sailor!"
+  log (show $ addu 5 6)
