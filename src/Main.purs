474f72a8ec3373df2c91bc033280bfeb2c3d757f
Chris LaRose <chris.larose@mycase.com>
2015-09-18 18:12:31 -0700

    cjl - Adding byte lists

--- src/Main.purs
+++ src/Main.purs
@@ -6,27 +6,87 @@ import Data.Foldable (foldr)
 import Data.List
 import Data.Tuple
 
-data UInt8 = UInt8 (Array Int)
-data UInt16 = UInt16 (Array Int)
-data UInt32 = UInt32 (Array Int)
-data UInt64 = UInt64 (Array Int)
-data UInt128 = UInt128 (Array Int)
+-- type UInt8 = Int
+data UInt8 = UInt8 Int
+data UInt16 = UInt16 UInt8 UInt8
+data UInt32 = UInt32 UInt16 UInt16
+data UInt64 = UInt64 UInt32 UInt32
+data UInt128 = UInt128 UInt64 UInt64
 
-instance showUInt64 :: Show UInt64 where
-  show (UInt64 bs) = show bs
+fromInt :: Int -> UInt8
+fromInt x = UInt8 (x `mod` 256)
 
-data BeltElement = NaR UInt64 | Result UInt64
+instance uInt8Show :: Show UInt8 where
+  show (UInt8 x) = "UInt8 " ++ show x
 
-instance showBeltElement :: Show BeltElement where
-  show (NaR x) = "NaR " ++ show x
-  show (Result x) = "Result " ++ show x
+class Bytes a where
+  toBytes :: a -> List Int
+  width :: Int
 
-addu :: List Int -> List Int -> List Int
-addu xs ys = (foldr f Nil (zip xs ys)) where
+addWithCarry :: forall b. (Bytes b) => b -> b -> (Tuple Boolean (List Int))
+addWithCarry a b = addLists' (toBytes a) (toBytes b)
+
+instance uInt8Bytes :: Bytes UInt8 where
+  toBytes (UInt8 a) = pure a
+  width = 1
+
+instance uInt16Bytes :: Bytes UInt16 where
+  toBytes (UInt16 a b) = (toBytes a) ++ (toBytes b)
+  width = 2
+
+instance uInt32Bytes :: Bytes UInt32 where
+  toBytes (UInt32 a b) = (toBytes a) ++ (toBytes b)
+  width = 4
+
+instance uInt64Bytes :: Bytes UInt64 where
+  toBytes (UInt64 a b) = (toBytes a) ++ (toBytes b)
+  width = 8
+
+instance uInt128Bytes :: Bytes UInt128 where
+  toBytes (UInt128 a b) = (toBytes a) ++ (toBytes b)
+  width = 16
+
+-- data UInt = UInt8 | UInt16 | UInt32 | UInt64 | UInt128
+
+-- addu :: forall a. (Bytes a) => a -> a -> a
+-- addu a b = (toBytes a) ++ (toBytes b)
+
+-- instance intBytes :: Bytes UInt26 where
+--   toBytes (U
+
+-- data BeltElement = NaR UInt32 | Result UInt32
+-- 
+-- instance showBeltElement :: Show BeltElement where
+--   show (NaR x) = "NaR " ++ show x
+--   show (Result x) = "Result " ++ show x
+-- 
+addLists :: List Int -> List Int -> List Int
+addLists xs ys = (foldr f Nil (zip xs ys)) where
   f (Tuple x y) Nil = ((x + y) / 256) : ((x + y) `mod` 256) : Nil
   f (Tuple x y) (Cons prevCarry rest) = let sum = x + y + prevCarry in
     (sum / 256) : (sum `mod` 256) : rest
 
+addLists' :: List Int -> List Int -> (Tuple Boolean (List Int))
+addLists' xs ys = (foldr f (Tuple false Nil) (zip xs ys)) where
+  f (Tuple x y) (Tuple carry prevSum) = (Tuple ((sum / 256) == 1) ((sum `mod` 256) : prevSum)) where
+    c = (if carry then 1 else 0)
+    sum = x + y + c
+    
+
+-- addu :: UInt32 -> UInt32 -> UInt32
+-- addu (UInt32 xs) (UInt32 ys) = case addLists xs ys of
+--                                     Nil -> UInt32 Nil
+--                                     (Cons _ rest) -> UInt32 rest
+
+-- modulo
+-- saturating
+-- excepting
+-- widening
+
 main = do
   -- log (show $ addu 5 6)
-  log (show $ addu (toList [1, 2, 3]) (toList [7, 8, 9]))
+  -- log (show $ addLists (toList [1, 2, 3]) (toList [7, 8, 9]))
+  log "hello world"
+  log (show $ fromInt 400)
+  -- log (show $ addu (UInt32 (toList [1, 2, 3])) (UInt32 (toList [7, 8, 9])))
+  -- log (show $ addu (UInt32 (toList [1])) (UInt32 (toList [255])))
