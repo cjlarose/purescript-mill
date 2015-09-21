4131ec1da5e38e94af96f1ac97310ce8a6116fba
Chris LaRose <chris.larose@mycase.com>
2015-09-20 21:28:17 -0700

    cjl - Unify semiring instances for all Bytes

--- src/ArithmeticContexts.purs
+++ src/ArithmeticContexts.purs
@@ -7,9 +7,7 @@ module ArithmeticContexts
 
 import Prelude
 import qualified Data.BigInt as BigInt
-import UnsignedInts (UInt8(), intToByte, byteToInt)
 import Bytes (Bytes, fromBigInt, toBigInt, clamp)
-import LargeKey (LargeKey(..))
 
 newtype ModularArithmetic a = ModularArithmetic a
 
@@ -41,25 +39,13 @@ instance applicativeSaturatingArithmetic :: Applicative SaturatingArithmetic whe
   pure = SaturatingArithmetic
 
 
-instance semiringModularArithmeticUInt8 :: Semiring (ModularArithmetic UInt8) where
-  add a b = intToByte <$> ((+) <$> (byteToInt <$> a) <*> (byteToInt <$> b))
-  zero = pure <<< intToByte $ 0
-  mul a b = intToByte <$> ((*) <$> (byteToInt <$> a) <*> (byteToInt <$> b))
-  one = pure <<< intToByte $ 1
-
-instance semiringSaturatingArithmeticUInt8 :: Semiring (SaturatingArithmetic UInt8) where
-  add a b = clamp <<< BigInt.fromInt <$> ((+) <$> (byteToInt <$> a) <*> (byteToInt <$> b))
-  zero = pure <<< intToByte $ 0
-  mul a b = clamp <<< BigInt.fromInt <$> ((*) <$> (byteToInt <$> a) <*> (byteToInt <$> b))
-  one = pure <<< intToByte $ 1
-
-instance semiringModularArithmeticLargeKey :: (Bytes a, Bytes b, Semiring (ModularArithmetic a), Semiring (ModularArithmetic b)) => Semiring (ModularArithmetic (LargeKey a b)) where
+instance semiringModularArithmeticBytes :: (Bytes a) => Semiring (ModularArithmetic a) where
   add x y = fromBigInt <$> ((+) <$> (toBigInt <$> x) <*> (toBigInt <$> y))
   zero = pure <<< fromBigInt $ zero :: BigInt.BigInt
   mul x y = fromBigInt <$> ((*) <$> (toBigInt <$> x) <*> (toBigInt <$> y))
   one = pure <<< fromBigInt $ one :: BigInt.BigInt
 
-instance semiringSaturatingArithmeticLargeKey :: (Bytes a, Bytes b, Semiring (SaturatingArithmetic a), Semiring (SaturatingArithmetic b)) => Semiring (SaturatingArithmetic (LargeKey a b)) where
+instance semiringSaturatingArithmeticBytes :: (Bytes a) => Semiring (SaturatingArithmetic a) where
   add x y = clamp <$> ((+) <$> (toBigInt <$> x) <*> (toBigInt <$> y))
   zero = pure <<< fromBigInt $ zero :: BigInt.BigInt
   mul x y = clamp <$> ((*) <$> (toBigInt <$> x) <*> (toBigInt <$> y))
