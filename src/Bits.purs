a4d6e7cc3afbc38beb08b12e2206b551b089f4ac
Chris LaRose <chris.larose@mycase.com>
2015-09-27 15:18:57 -0700

    cjl - Add instance of Bits for UInt8

--- src/Bits.purs
+++ src/Bits.purs
@@ -11,9 +11,14 @@ module Bits
   , rotate
   , testBit
   , popCount
+  , popCountLookupTable
   ) where
 
 import Prelude
+import Data.Array ((..))
+import Data.Array.Unsafe (unsafeIndex)
+import qualified Data.Int.Bits as IntBits
+import UnsignedInts (UInt8(), intToByte, byteToInt)
 
 class (Eq a) <= Bits a where
   and :: a -> a -> a
@@ -33,3 +38,26 @@ class (Eq a) <= Bits a where
 
 (.^.) :: forall a. (Bits a) => a -> a -> a
 (.^.) = xor
+
+popCountLookupTable :: Array Int
+popCountLookupTable = map countBits (0 .. 255) where
+  countNibble = [0, 1, 1, 2, 1, 2, 2, 3,
+                 1, 2, 2, 3, 2, 3, 3, 4]
+  countBits x = countNibble `unsafeIndex` (IntBits.zshr x 4) + countNibble `unsafeIndex` (IntBits.(.&.) x 0x0F)
+
+instance bitsUInt8 :: Bits UInt8 where
+  and a b = intToByte $ IntBits.(.&.) (byteToInt a) (byteToInt b)
+  or a b = intToByte $ IntBits.(.|.) (byteToInt a) (byteToInt b)
+  xor a b = intToByte $ IntBits.(.^.) (byteToInt a) (byteToInt b)
+  complement = intToByte <<< IntBits.complement <<< byteToInt
+  shift x n | n > 0 = intToByte $ IntBits.zshr (byteToInt x) n
+            | n < 0 = intToByte $ IntBits.shl (byteToInt x) n
+            | otherwise = x
+  rotate x n = intToByte masked where
+    ix = (byteToInt x)
+    repeated = IntBits.(.|.) (IntBits.shl ix 8) ix
+    shifted = IntBits.zshr repeated (n `mod` 8)
+    masked = IntBits.(.|.) shifted 0xFF
+  testBit x n = IntBits.(.&.) shifted 1 == 1 where
+    shifted = IntBits.zshr (byteToInt x) n
+  popCount = unsafeIndex popCountLookupTable <<< byteToInt
