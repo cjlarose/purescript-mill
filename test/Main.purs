c863e8d4c0078e3e9e1de3d3d70aff197a6ae4fb
Chris LaRose <chris.larose@mycase.com>
2015-10-04 13:41:17 -0700

    cjl - Add tests for modular arithmetic semigroup instance

--- test/Main.purs
+++ test/Main.purs
@@ -4,8 +4,21 @@ import Control.Monad.Eff.Console
 import Test.QuickCheck (quickCheck, (===))
 import Prelude
 import ArithmeticContexts (ModularArithmetic(..), SaturatingArithmetic(..))
-import UnsignedInts (intToByte)
+import UnsignedInts (intToByte, UInt8())
 
 main = do
-  quickCheck \n -> n + (ModularArithmetic (intToByte 1)) === (ModularArithmetic (intToByte 1)) + n
-  quickCheck \n -> n + (SaturatingArithmetic (intToByte 1)) === (SaturatingArithmetic (intToByte 1)) + n
+  -- modular addition of UInt8 forms a communtative monoid
+  quickCheck \a b c -> (a :: ModularArithmetic UInt8 + b) + c === a + (b + c)
+  quickCheck \a -> (a :: ModularArithmetic UInt8) + zero === zero + a
+  quickCheck \a -> (a :: ModularArithmetic UInt8) + zero === a
+  quickCheck \a b -> (a :: ModularArithmetic UInt8) + b === b + a
+  -- modular multiplication of UInt8 forms a monoid
+  quickCheck \a b c -> (a :: ModularArithmetic UInt8 * b) * c === a * (b * c)
+  quickCheck \a -> (a :: ModularArithmetic UInt8) * one === one * a
+  quickCheck \a -> (a :: ModularArithmetic UInt8) * one === a
+  -- modular mulitplication distributes over modular addition
+  quickCheck \a b c -> (a :: ModularArithmetic UInt8) * (b + c) === a * b + a * c
+  quickCheck \a b c -> ((a :: ModularArithmetic UInt8) + b) * c === a * c + b * c
+  -- annihilation
+  quickCheck \a -> (a :: ModularArithmetic UInt8) * zero === zero * a
+  quickCheck \a -> (a :: ModularArithmetic UInt8) * zero === zero
