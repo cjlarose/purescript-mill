b7846a1f3ec3e3fa51b1402bc33273158e4abb47
Chris LaRose <chris.larose@mycase.com>
2015-10-04 13:59:18 -0700

    cjl - Add saturating arithmetic tests for UInt8

--- test/Main.purs
+++ test/Main.purs
@@ -23,5 +23,23 @@ modularArithmetic = do
   quickCheck \a -> (a :: ModularArithmetic UInt8) * zero === zero * a
   quickCheck \a -> (a :: ModularArithmetic UInt8) * zero === zero
 
+saturatingArithmetic = do
+  -- saturating addition of UInt8 forms a communtative monoid
+  quickCheck \a b c -> (a :: SaturatingArithmetic UInt8 + b) + c === a + (b + c)
+  quickCheck \a -> (a :: SaturatingArithmetic UInt8) + zero === zero + a
+  quickCheck \a -> (a :: SaturatingArithmetic UInt8) + zero === a
+  quickCheck \a b -> (a :: SaturatingArithmetic UInt8) + b === b + a
+  -- saturating multiplication of UInt8 forms a monoid
+  quickCheck \a b c -> (a :: SaturatingArithmetic UInt8 * b) * c === a * (b * c)
+  quickCheck \a -> (a :: SaturatingArithmetic UInt8) * one === one * a
+  quickCheck \a -> (a :: SaturatingArithmetic UInt8) * one === a
+  -- saturating mulitplication distributes over saturating addition
+  quickCheck \a b c -> (a :: SaturatingArithmetic UInt8) * (b + c) === a * b + a * c
+  quickCheck \a b c -> ((a :: SaturatingArithmetic UInt8) + b) * c === a * c + b * c
+  -- annihilation
+  quickCheck \a -> (a :: SaturatingArithmetic UInt8) * zero === zero * a
+  quickCheck \a -> (a :: SaturatingArithmetic UInt8) * zero === zero
+
 main = do
   modularArithmetic
+  saturatingArithmetic
