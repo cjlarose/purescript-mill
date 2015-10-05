76d9877471ce727f68222bd6d3ef07c6e2445788
Chris LaRose <chris.larose@mycase.com>
2015-10-04 18:29:09 -0700

    cjl - Better test output

--- test/Main.purs
+++ test/Main.purs
@@ -11,83 +11,99 @@ import Bits ((.^.))
 (.=>.) p q  = (not p) || q
 
 modularArithmetic = do
-  -- modular addition of UInt8 forms a communtative monoid
+  log "modular addition of UInt8 forms a communtative monoid"
   quickCheck \a b c -> (a :: ModularArithmetic UInt8 + b) + c === a + (b + c)
   quickCheck \a -> (a :: ModularArithmetic UInt8) + zero === zero + a
   quickCheck \a -> (a :: ModularArithmetic UInt8) + zero === a
   quickCheck \a b -> (a :: ModularArithmetic UInt8) + b === b + a
-  -- modular multiplication of UInt8 forms a monoid
+
+  log "modular multiplication of UInt8 forms a monoid"
   quickCheck \a b c -> (a :: ModularArithmetic UInt8 * b) * c === a * (b * c)
   quickCheck \a -> (a :: ModularArithmetic UInt8) * one === one * a
   quickCheck \a -> (a :: ModularArithmetic UInt8) * one === a
-  -- modular mulitplication distributes over modular addition
+
+  log "modular mulitplication distributes over modular addition"
   quickCheck \a b c -> (a :: ModularArithmetic UInt8) * (b + c) === a * b + a * c
   quickCheck \a b c -> ((a :: ModularArithmetic UInt8) + b) * c === a * c + b * c
-  -- annihilation
+
+  log "annihilation"
   quickCheck \a -> (a :: ModularArithmetic UInt8) * zero === zero * a
   quickCheck \a -> (a :: ModularArithmetic UInt8) * zero === zero
 
 saturatingArithmetic = do
-  -- saturating addition of UInt8 forms a communtative monoid
+  log "saturating addition of UInt8 forms a communtative monoid"
   quickCheck \a b c -> (a :: SaturatingArithmetic UInt8 + b) + c === a + (b + c)
   quickCheck \a -> (a :: SaturatingArithmetic UInt8) + zero === zero + a
   quickCheck \a -> (a :: SaturatingArithmetic UInt8) + zero === a
   quickCheck \a b -> (a :: SaturatingArithmetic UInt8) + b === b + a
-  -- saturating multiplication of UInt8 forms a monoid
+
+  log "saturating multiplication of UInt8 forms a monoid"
   quickCheck \a b c -> (a :: SaturatingArithmetic UInt8 * b) * c === a * (b * c)
   quickCheck \a -> (a :: SaturatingArithmetic UInt8) * one === one * a
   quickCheck \a -> (a :: SaturatingArithmetic UInt8) * one === a
-  -- saturating mulitplication distributes over saturating addition
+
+  log "saturating mulitplication distributes over saturating addition"
   quickCheck \a b c -> (a :: SaturatingArithmetic UInt8) * (b + c) === a * b + a * c
   quickCheck \a b c -> ((a :: SaturatingArithmetic UInt8) + b) * c === a * c + b * c
-  -- annihilation
+
+  log "annihilation"
   quickCheck \a -> (a :: SaturatingArithmetic UInt8) * zero === zero * a
   quickCheck \a -> (a :: SaturatingArithmetic UInt8) * zero === zero
 
 eqLaws = do
-  -- reflexivity
+  log "reflexivity"
   quickCheck \a -> (a :: UInt8) === a
-  -- symmetry
+
+  log "symmetry"
   quickCheck \a b -> eq (a :: UInt8) b === eq b a
-  -- transitivity
+
+  log "transitivity"
   quickCheck \a b c -> ((eq (a :: UInt8) b) && eq b c) .=>. (eq a c)
 
 ordLaws = do
-  -- reflexivity
+  log "reflexivity"
   quickCheck \a -> (a :: UInt8) <= a
-  -- antisymmetry
+
+  log "antisymmetry"
   quickCheck \a b -> (a :: UInt8 <= b && b <= a) .=>. a == b
-  -- transitivity
+
+  log "transitivity"
   quickCheck \a b c -> (a :: UInt8 <= b && b <= c) .=>. a <= c
 
 boundedOrdLaws = do
-  -- ordering
+  log "ordering"
   quickCheck \a -> bottom <= a :: UInt8 && a <= top
 
 booleanAlgebraLaws = do
-  -- associativity
+  log "associativity"
   quickCheck \a b c -> a :: UInt8 || (b || c) === (a || b) || c
   quickCheck \a b c -> a :: UInt8 && (b && c) === (a && b) && c
-  -- commutativity
+
+  log "commutativity"
   quickCheck \a b -> a :: UInt8 || b === b || a
   quickCheck \a b -> a :: UInt8 && b === b && a
-  -- distributivity
+
+  log "distributivity"
   quickCheck \a b c -> a :: UInt8 && (b || c) === a && b || a && c
   quickCheck \a b c -> a :: UInt8 || (b && c) === (a || b) && (a || c)
-  -- idempotence
+
+  log "idempotence"
   quickCheck \a -> a :: UInt8 || a === a
   quickCheck \a -> a :: UInt8 && a === a
-  -- absorption
+
+  log "absorption"
   quickCheck \a b -> a :: UInt8 || (a && b) === a
   quickCheck \a b -> a :: UInt8 && (a || b) === a
-  -- annihilation
+
+  log "annihilation"
   quickCheck \a -> a :: UInt8 || top === top
-  --- complementation
+
+  log "complementation"
   quickCheck \a -> a :: UInt8 && (not a) === bottom
   quickCheck \a -> a :: UInt8 || (not a) === top
 
 bitsLaws = do
-  -- xor law
+  log "xor law"
   quickCheck \a b -> (a :: UInt8 .^. b) === (a || b) && (not (a && b))
 
 main = do
